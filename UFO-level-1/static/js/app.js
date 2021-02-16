2809b3c71569a8163a144f1a776dd46c6c387d0c
Alex Torres <62618236+alextorres12@users.noreply.github.com>
2021-02-16 01:35:17 -0500

    adds event listeners and prevents refreshing

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,6 +2,26 @@
 var tableData = data;
 console.log("data :: ", data);
 // YOUR CODE HERE!
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers
+button.on("click", filterData);
+form.on("submit", filterData);
+
+// Function to filter data
+function filterData() {
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  var inputValue = d3.select("#datetime").property("value");
+
+  console.log("you clicked the button");
+  console.log("you entered: ", inputValue);
+};
 const tableBody = d3.select("#ufo-table tbody");
 
 // tableBody.html = "";
